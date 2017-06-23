c8845da62f2968c4dd1857b2e61921a551e42227
Rinat Kashapov <rkashapov85@gmail.com>
2017-06-23 16:10:26 +0300

    23062017

--- autoload.php
+++ autoload.php
@@ -2,7 +2,7 @@
 spl_autoload_register(function ($class_name) {
     
     $filename = str_replace('\\', DIRECTORY_SEPARATOR, $class_name) . '.php';
-    $path_lib = '/var/lib/phpagi/';
+    $path_lib = 'common/';
     $file = $path_lib . $filename;
     if (file_exists($file) === false) {
         throw new Exception('Undefined class', 500);
